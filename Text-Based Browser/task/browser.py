e2d9d47dd3c8abe42fbcfdc1865ea0ae622a8450
Ignat <ignativanov1996@mail.ru>
2021-04-22 11:30:12 +0300

    2021.04.22.1129
    Project complete. Some problems solved.

--- Text-Based Browser/task/browser.py
+++ Text-Based Browser/task/browser.py
@@ -1,73 +1,46 @@
 import argparse
 import requests
-import collections
 import os
 from bs4 import BeautifulSoup
-from colorama import Fore
+from colorama import Fore, init
 
-nytimes_com = r'''
-This New Liquid Is Magnetic, and Mesmerizing
-
-Scientists have created “soft” magnets that can flow 
-and change shape, and that could be a boon to medicine 
-and robotics. (Source: New York Times)
-
-
-Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
-
-Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
-
-'''
-
-bloomberg_com = """
-The Space Race: From Apollo 11 to Elon Musk
-
-It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
-
-
-Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
-
-Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.
-"""
 
 history_stack = []  # Stack for browser history in current session
 
 
 def save_page(name, site):
     file = open(save_dir + '\\' + name, 'w', encoding='utf-8')
-    file.write(site)
+    file.write(str(site))
     file.close()
 
 
 def print_page(name):
     file = open(save_dir + '\\' + name, 'r')
-    print(*file.readlines())
+    soup_string = ''
+    for line in file.readlines():
+        soup_string += line
+    soup = BeautifulSoup(soup_string, 'html.parser')
+    site_content = soup.find_all(["p", 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'ul', 'ol', 'li'])
+    print_text(site_content)
     file.close()
 
 
 def extract_content(url):
-    site_content = ''
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
-    site_content = soup.get_text()
-    site_list = site_content.split('\n')
-    site_content = ''
-    for line in site_list:
-        if line == '':
-            pass
-        else:
-            site_content += line + '\n'
+    site_content = soup.find_all(["p", 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'ul', 'ol', 'li'])
     return site_content
+
+
+def print_text(site_content):
+    init(autoreset=True)
+    for line in site_content:
+        print(line)
+        if str(line).find('href') >= 0:
+            print(Fore.BLUE + line.get_text(), sep='\n')
+        else:
+            print(line.get_text(strip=True), sep='\n')
+            print(line.get_text(), sep='\n')
 
 
 parser = argparse.ArgumentParser()  # Need parser for handling run parameters
@@ -103,9 +76,7 @@ while url != 'exit':
             prefix = ''
             if url[8:] != 'https://':
                 prefix = 'https://'
-            # r = requests.get(prefix + url).content
-            # r = requests.get(prefix + url)
             site_text = extract_content(prefix + url)
-            print(site_text)
+            print_text(site_text)
             save_page(url[:url.find('.')], site_text)
             prev_url = url
